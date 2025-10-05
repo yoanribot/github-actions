cf61ca7721b60563184deaefe7b06dee59034889
yRibot <yoanribot@gmail.com>
2025-10-06 00:21:02 +0200

    Add identity-obj-proxy for CSS mocking and fileMock.js to mock static assets

--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,8 @@ export default {
   transform: {
     ...tsJestTransformCfg,
   },
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/src/__mocks__/fileMock.js",
+  },
 };
