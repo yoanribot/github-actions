ca52ca4f7110276cb63c0b5e31fb956ab206292e
yRibot <yoanribot@gmail.com>
2025-10-06 22:29:52 +0200

    Update Jest configuration to include coverage settings and report formats

--- jest.config.js
+++ jest.config.js
@@ -14,4 +14,12 @@ export default {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
     "\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/src/__mocks__/fileMock.js",
   },
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/main.tsx",
+    "!src/vite-env.d.ts",
+  ],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "html"],
 };
