93ea23e25757a1cd8df75db710a36cd1dd17adaf
yRibot <yoanribot@gmail.com>
2025-10-08 23:52:33 +0200

    Add Playwright testing framework and configuration

---
+++ e2e/example.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/github-actions/);
+  await expect(
+    page.getByRole("heading", { name: "Vite + React" })
+  ).toBeVisible();
+});
